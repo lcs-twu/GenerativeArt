79c0fe1b92db327da67b4531916652fa56ddaa9c
Tom Wu <twu@lcs.on.ca>
2023-10-24 11:30:05 -0400

    Initial Commit

---
+++ GenerativeArt/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  GenerativeArt
+//
+//  Created by Tom Wu on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
