4b44b43729c48dceaffe591190f7fc10831952ec
Tom Wu <twu@lcs.on.ca>
2023-10-24 11:55:46 -0400

    drawing top right triangle with path

--- GenerativeArt/Views/TitleView.swift
+++ GenerativeArt/Views/TitleView.swift
@@ -7,6 +7,26 @@
 
 import SwiftUI
 
+//create shape structure
+struct TriangleTopRight: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        //make an empty path
+        var path = Path()
+        //define path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        //side one
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        //side two
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        //side three
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        //return the path
+        return path
+    }
+    
+}
+
 struct TitleView: View {
     var body: some View {
         VStack {
@@ -14,6 +34,7 @@ struct TitleView: View {
             //make the shape to be a square
                 .aspectRatio(1.0, contentMode: .fit)
         }
+        .padding()
     }
 }
 
