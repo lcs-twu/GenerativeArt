c538bbfa9cd72908e1e7d9f8b635dcc235488d24
Tom Wu <twu@lcs.on.ca>
2023-10-24 12:00:31 -0400

    TriangleBottomLeft

--- GenerativeArt/Views/TitleView.swift
+++ GenerativeArt/Views/TitleView.swift
@@ -27,12 +27,37 @@ struct TriangleTopRight: Shape {
     
 }
 
+struct TriangleBottomLeft: Shape {
+    func path(in rect: CGRect) -> Path {
+        //make an empty path
+        var path = Path()
+        //define path
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        //side one
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        //side two
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        //side three
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        //return the path
+        return path
+    }
+}
+
+
 struct TitleView: View {
     var body: some View {
         VStack {
-            Rectangle()
-            //make the shape to be a square
-                .aspectRatio(1.0, contentMode: .fit)
+            let coinFlipOne = Int.random(in: 1...2)
+            
+            if coinFlipOne == 1{
+                TriangleTopRight()
+                //make the shape to be a square
+                    .aspectRatio(1.0, contentMode: .fit)
+            } else {
+                TriangleBottomLeft()
+                    .aspectRatio(1.0, contentMode: .fit)
+            }
         }
         .padding()
     }
